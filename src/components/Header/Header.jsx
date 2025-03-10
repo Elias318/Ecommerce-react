aedb9708b0b096701a00baa60d82b91fcd62ae2f
Elias Olivencia <elias.olivencia@gmail.com>
2025-03-10 20:04:04 -0300

    Creacion de proyecto

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+
+import '../Header/header.css'
+function Header() {
+ 
+
+  return (
+    <>
+        <div className="container_navbar">
+            <div className="container_logo">
+                <h2>LOGO</h2>
+            </div>
+
+            <div className="container_menu_header">
+
+            </div>
+
+            <div className="container_carrito">
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Header
